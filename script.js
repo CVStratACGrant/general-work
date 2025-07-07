d4f5bfe459d8927a55d6a21be6db3e5bfb4fdcdd
CV Strategies <cvstrategies@macbookpro.lan>
2025-07-07 13:47:38 -0700

    make function robust and check levels

--- script.js
+++ script.js
@@ -26,16 +26,21 @@ class WidgetBase extends HTMLElement {
     }
 
     async fetchWaterLevel () {
-        //returns promise because its async 
         const response = await fetch('https://waterservices.usgs.gov/nwis/iv/?sites=11109700&agencyCd=USGS&parameterCd=00054&period=P7D&siteStatus=all&format=json');
         const data = await response.json();
         const dataLength = data.value.timeSeries[0].values[0].value.length;
         const mostRecent = data.value.timeSeries[0].values[0].value[dataLength-1].value;
 
         // 1 -(water level / 83240 acre feet) = percent below capacity 
-        const currWaterLevel = 1-(mostRecent/83240)
+        const percentToCapacity = (1 - (mostRecent / 83240)) * 100
+        this.getWhitewaterRaftingStatus(percentToCapacity);
     }
 
+    getWhitewaterRaftingStatus (percentToCapacity) {
+        if (percentToCapacity > 90) console.log('Water is about to be released. Whitewater rafting is closed.', percentToCapacity);
+        else if (percentToCapacity > 70) console.log('Water is being released. Whitewater rafting is open.', percentToCapacity);
+        else console.log('Water has been released. Whitewater rafting is closed.', percentToCapacity);
+    }
 
 }
 
