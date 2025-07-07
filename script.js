594ff9354d09fa643eab5f27410c33964500c6f9
nat-cvstrat <nat@cvstrat.com>
2025-07-07 12:58:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//https://waterservices.usgs.gov/nwis/iv/?sites=11109700&agencyCd=USGS&parameterCd=00054&period=P7D&siteStatus=all&format=json
 
 class WidgetBase extends HTMLElement {
     constructor () {
@@ -15,13 +15,25 @@ class WidgetBase extends HTMLElement {
 
             <div>
                 <h1>Hello</h1>
+                <button id="fetch-data-button">Fetch Data</button>
             </div>
         `;
     }
 
 
     connectedCallback () {
-        
+        this.shadowRoot.getElementById('fetch-data-button').addEventListener('click', () => this.fetchWaterLevel())
+    }
+
+    async fetchWaterLevel () {
+        //returns promise because its async 
+        const response = await fetch('https://waterservices.usgs.gov/nwis/iv/?sites=11109700&agencyCd=USGS&parameterCd=00054&period=P7D&siteStatus=all&format=json');
+        const data = await response.json();
+        const dataLength = data.value.timeSeries[0].values[0].value.length;
+        const mostRecent = data.value.timeSeries[0].values[0].value[dataLength-1].value;
+
+        // 1 -(water level / 83240 acre feet) = percent below capacity 
+        const currWaterLevel = 1-(mostRecent/83240)
     }
 
 
